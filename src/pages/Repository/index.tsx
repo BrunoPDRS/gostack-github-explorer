a0a0b33145e18dc289115fae470977b4d1eb53f4
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-01-04 12:39:45 -0300

    Styling Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo-github-explorer.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,51 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>a</strong>
+            <p>a</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>asasa</strong>
+            <p>sasas</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
